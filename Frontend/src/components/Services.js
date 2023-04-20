763001b858b02804713d192a395e00aa43b35710
Alve <alvin.nayak2@gmail.com>
2023-04-21 00:09:50 +0530

    Navbar_Alignment

--- Frontend/src/components/Services.js
+++ Frontend/src/components/Services.js
@@ -41,10 +41,10 @@ const Services = () => {
                             </Link>
                         </div>
                         <div className='relative'>To perform Pooja in Sambalpur</div>
-                        <div className='flex flex-row items-center gap-4 relative'>
+                        <div className='flex flex-row items-center gap-4 relative ml-6'>
                             <div className='flex flex-col relative'>
-                                My Pandit Preference
-                                <select className='' name='' id=''>
+                                <div className='mb-2'>My Pandit Preference</div>
+                                <select className='rounded-lg' name='' id=''>
                                     <option value='1'>Select</option>
                                     <option value='2'>Odia</option>
                                     <option value='3'>Hindi</option>
@@ -52,25 +52,25 @@ const Services = () => {
                                     <option value='5'>Gujrati</option>
                                 </select>
                             </div>
-                            <div className="relative gap-2">
+                            <div className="relative ml-6">
                                 <Link to='/services'>
-                                    <button className='font-serif font-bold shadow-lg hover:opacity-75 bg-black text-white  bg-blend-multiply box-border h-9 w-52 rounded-md cursor-pointer'>
+                                    <button className='font-sans font-semibold shadow-lg hover:opacity-75 bg-black text-white bg-blend-multiply box-border py-2 px-4 rounded-md cursor-pointer'>
                                         View All Services
                                     </button>
                                 </Link>
                             </div>
                         </div>
-                        <div className="relative gap-2">
+                        <div className="relative gap-2 ml-6">
                             <input class="block w-full py-2 pr-10 pl-4 mrleading-tight rounded-lg border border-gray-300 bg-white focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Search..." />
                             <button class="absolute top-0 right-0 px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded-lg">
                                 Search
                             </button>
                         </div>
-                        <div className='flex flex-row justify-end gap-5 mt-3 ml-10'>
-                            <button className='hover:opacity-75 shadow-lg bg-black text-white shadow-black box-border h-7 w-20 pr-7 pl-5 pb-3 rounded-md relative'>
+                        <div className='flex flex-row justify-end gap-5 ml-10'>
+                            <button className='hover:opacity-75 shadow-lg bg-black text-white shadow-black box-border py-2 px-4 pb-3 rounded-md relative'>
                                 Login
                             </button>
-                            <button className='hover:opacity-75 shadow-lg bg-black text-white shadow-black box-border h-7 w-20 pb-2 rounded-md relative'>
+                            <button className='hover:opacity-75 shadow-lg bg-black text-white shadow-black box-border py-2 px-4 rounded-md relative'>
                                 Register
                             </button>
                         </div>
