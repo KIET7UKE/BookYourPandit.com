9ffddcffff2c7b3dcaf03d93031a1ff15d5794d5
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2023-10-01 15:15:49 +0530

    Revert "folder structure"

--- src/middlewares/errorMiddleware.js
+++
@@ -1,5 +0,0 @@
-const errorMiddleware = (error, req, res, next) => {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
-};
-module.exports = errorMiddleware;
