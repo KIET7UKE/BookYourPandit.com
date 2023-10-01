ccb2b433d10d66f141c93281e2c6a5f5fc11e68b
KIET7UKE <sharmasahil2468@gmail.com>
2023-10-01 16:43:50 +0530

    Added Server API

--- src/Commons/axios.js
+++ src/Commons/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: 'http://localhost:3000/api',
-  //   baseURL: '', //server API
+  //   baseURL: 'https://byp-server.onrender.com/api', //server API
   headers: { 'X-Custom-Header': 'foobar' },
 });
