8d2e9eca04cca06345ae7db2e60a12fc37d5dc6d
Krupal Bavishi <krupalbavishi004@gmail.com>
2023-10-02 11:58:38 +0530

    register+login

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors =  require('cors');
 const bodyParser =  require('body-parser');
 require('./src/config/connect.js');
 const errorMiddleware = require("./src/middlewares/errorMiddleware.js")
+const userRoutes = require("./src/routes/User/userRoutes.js")
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -17,5 +18,6 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(errorMiddleware);
+app.use([userRoutes])
 
 app.listen(PORT, () => console.log('Server is running on PORT : ' + PORT));
